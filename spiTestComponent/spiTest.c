7b14c83ba04c5150be36f6413ad0be287a1bcdbe
coastalbrandon <brandon@brnkl.io>
2017-09-24 19:36:47 -0700

    Update names of functions

--- spiTestComponent/spiTest.c
+++ spiTestComponent/spiTest.c
@@ -28,24 +28,26 @@ COMPONENT_INIT
 
     LE_INFO("===========> SPI application has started");
 
-    le_spi_DeviceHandleRef_t spiHandle;
+    spi_DeviceHandleRef_t spiHandle;
 
     LE_INFO("Opening SPI file handle");
-    res = le_spi_Open("spidev0.0", &spiHandle);
+    res = spi_Open("spidev0.0", &spiHandle);
     LE_FATAL_IF(res != LE_OK, "le_spi_Open failed with result=%s", LE_RESULT_TXT(res));
 
     LE_INFO("Configuring SPI");
-    le_spi_Configure(spiHandle, 0, 8, 1000000, 0);
+    spi_Configure(spiHandle, 0, 8, 1000000, 0);
 
     LE_INFO("Reading from the MCP3004 specified channel");
     size_t readBufferSize = NUM_ARRAY_MEMBERS(rxData);    
-    le_spi_WriteReadHD(spiHandle, txData, NUM_ARRAY_MEMBERS(txData), rxData, &readBufferSize);
+    spi_WriteReadHD(spiHandle, txData, NUM_ARRAY_MEMBERS(txData), rxData, &readBufferSize);
     LE_FATAL_IF(res != LE_OK, "le_spi_WriteReadHD failed with result=%s", LE_RESULT_TXT(res));
     
-    a2dVal = (rxData[1] << 4) & 0b1100000000; //merge rxData[1] and rxData[2] to get result
-    a2dVal |= (rxData[2] & 0xff);
+    //a2dVal = (rxData[1] << 4) & 0b1100000000; //merge rxData[1] and rxData[2] to get result
+    //a2dVal |= (rxData[2] & 0xff);
+
+    a2dVal = rxData[1];
     
     LE_INFO("A2D Value= %d", a2dVal);
 
-    le_spi_Close(spiHandle);
+    spi_Close(spiHandle);
 }
