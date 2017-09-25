279577b07a290e7afb51aa8e1438807bbf4c8eab
coastalbrandon <brandon@brnkl.io>
2017-09-24 19:18:59 -0700

    changed spi API directory updated spi functions

---
+++ _build_spiTest/wp85/component/e8846d2f677e31ab648379826a403b30/src/_componentMain.c
@@ -0,0 +1,44 @@
+/*
+ * AUTO-GENERATED _componentMain.c for the spiTestComponent component.
+
+ * Don't bother hand-editing this file.
+ */
+
+#include "legato.h"
+#include "../src/eventLoop.h"
+#include "../src/log.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const char* _spiTestComponent_spi_ServiceInstanceName;
+const char** spi_ServiceInstanceNamePtr = &_spiTestComponent_spi_ServiceInstanceName;
+void spi_ConnectService(void);
+// Component log session variables.
+le_log_SessionRef_t spiTestComponent_LogSession;
+le_log_Level_t* spiTestComponent_LogLevelFilterPtr;
+
+// Component initialization function (COMPONENT_INIT).
+void _spiTestComponent_COMPONENT_INIT(void);
+
+// Library initialization function.
+// Will be called by the dynamic linker loader when the library is loaded.
+__attribute__((constructor)) void _spiTestComponent_Init(void)
+{
+    LE_DEBUG("Initializing spiTestComponent component library.");
+
+    // Connect client-side IPC interfaces.
+    spi_ConnectService();
+
+    // Register the component with the Log Daemon.
+    spiTestComponent_LogSession = log_RegComponent("spiTestComponent", &spiTestComponent_LogLevelFilterPtr);
+
+    //Queue the COMPONENT_INIT function to be called by the event loop
+    event_QueueComponentInit(_spiTestComponent_COMPONENT_INIT);
+}
+
+
+#ifdef __cplusplus
+}
+#endif
