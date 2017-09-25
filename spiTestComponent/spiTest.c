5911cb033f9e0970c331c21ff42ca80af2cb63db
coastalbrandon <brandon@brnkl.io>
2017-09-25 16:33:33 -0700

    successfully reading mcp3008

--- spiTestComponent/spiTest.c
+++ spiTestComponent/spiTest.c
@@ -9,19 +9,23 @@
 #include "legato.h"
 #include "interfaces.h"
 
+le_spi_DeviceHandleRef_t spiHandle;
+
 unsigned char chanBits;
 uint8_t chan = 0; //read from channel 1
 uint32_t a2dVal = 0;
+float voltage = 0.0;
 
 uint8_t rxData [3];
 uint8_t txData [3];
 
 le_result_t res;
 
-COMPONENT_INIT
+void pollMCP
+(
+	le_timer_Ref_t spiTimerRef
+)
 {
-
-    //chanBits = 0b10000000 | (chan << 4) ;
     chanBits = (~0) << 7;
     chanBits = chanBits | (chan << 4);
 
@@ -30,26 +34,14 @@ COMPONENT_INIT
     txData [1] |= (chan << 4);
     txData [2] = 0x0; //dont care
 
-
-    LE_INFO("===========> SPI application has started");
-
-    le_spi_DeviceHandleRef_t spiHandle;
-
-    LE_INFO("Opening SPI file handle");
-    res = le_spi_Open("spidev0.0", &spiHandle);
-    LE_FATAL_IF(res != LE_OK, "le_spi_Open failed with result=%s", LE_RESULT_TXT(res));
-
-    LE_INFO("Configuring SPI");
-    le_spi_Configure(spiHandle, 0, 8, 1000000, 0);
-
-
-    LE_INFO("Reading from the MCP3004 specified channe");
-    size_t readBufferSize = NUM_ARRAY_MEMBERS(rxData);    
-    le_spi_WriteReadHD(spiHandle, txData, NUM_ARRAY_MEMBERS(txData), rxData, &readBufferSize);
+    LE_INFO("Reading MCP at channel %d: ", chan);
+    size_t readBufferSize = NUM_ARRAY_MEMBERS(rxData);
+    res = le_spi_WriteReadFD(spiHandle, txData, NUM_ARRAY_MEMBERS(txData), rxData, &readBufferSize);
     LE_FATAL_IF(res != LE_OK, "le_spi_WriteReadHD failed with result=%s", LE_RESULT_TXT(res));
 
     a2dVal = rxData[1] & 0x03;
     a2dVal = (a2dVal << 8) | rxData[2];
+    voltage = a2dVal * 0.0036 - 0.6825;
 
     LE_INFO("txData[0]= %d", txData[0]);
     LE_INFO("txData[1]= %d", txData[1]);
@@ -59,8 +51,31 @@ COMPONENT_INIT
     LE_INFO("rxData[1]= %d", rxData[1]);
     LE_INFO("rxData[2]= %d", rxData[2]);
     LE_INFO("A2D = %d", a2dVal);
+    LE_INFO("VOLTAGE = %f", voltage);
+ 
+}
+
+COMPONENT_INIT
+{
+
+    LE_INFO("===========> SPI application has started");
+
+    LE_INFO("Opening SPI file handle");
+    res = le_spi_Open("spidev0.0", &spiHandle);
+    LE_FATAL_IF(res != LE_OK, "le_spi_Open failed with result=%s", LE_RESULT_TXT(res));
+
+    LE_INFO("Configuring SPI");
+    le_spi_Configure(spiHandle, 0, 8, 960000, 0);
+
+
+    le_timer_Ref_t spiTimerRef = le_timer_Create("SPI Timer");
+    le_timer_SetMsInterval(spiTimerRef, 1000);
+    le_timer_SetRepeat(spiTimerRef, 0);
+
+    le_timer_SetHandler(spiTimerRef,pollMCP);
+    le_timer_Start(spiTimerRef);
     
 
-    LE_INFO("CLOSING SPI <======================");
-    le_spi_Close(spiHandle);
+    //LE_INFO("CLOSING SPI <======================");
+    //le_spi_Close(spiHandle);
 }
